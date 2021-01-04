054cb37d7c090d42482e894d213fd26e8d0e1b4d
PaddiM8 <=>
2021-01-05 00:44:57 +0100

    gulp

---
+++ gulpfile.esm.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import { task, src, dest, watch, parallel } from 'gulp';
+import sass, { compiler, logError } from 'gulp-sass';
+import inlineSource from 'gulp-inline-source-html'
+
+compiler = require('sass');
+
+task('sass', async () => {
+    return src('./style/**/*.sass')
+        .pipe(sass().on('error', logError))
+        .pipe(dest('./public/dist'));
+});
+
+task('inlineSource', async () => {
+    return src('*.html')
+        .pipe(inlineSource({ compress: true }))
+        .pipe(dest('public'));
+});
+
+task('copyKalkComponent', async () => {
+    return src('./node_modules/@paddim8/kalk-component/public/build/*')
+        .pipe(dest('./public/dist/'));
+});
+
+task('watch', () => {
+    watch('./style/**/*.sass', ['sass']);
+
+});
+
+task('default', done => {
+    parallel('sass', 'inlineSource', 'copyKalkComponent');
+    done();
+});
